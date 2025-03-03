d37ec764a25b1a6a8f9022c60fd4ad95773e052f
lbeneylton <47756391+lbeneylton@users.noreply.github.com>
2025-03-03 18:01:52 +0000

    ajeitei a entrada da nota 1

--- Hora de codar/media.c
+++ Hora de codar/media.c
@@ -3,8 +3,9 @@
 
 
 int main(){
-    float nota1, nota2, nota3, media;
+    double nota1, nota2, nota3, media;
     printf("Entre com a primeira nota do aluno\n");
+    scanf(" %lf", &nota1);
 
     printf("\nEntre com a segunda nota do aluno\n");
     scanf(" %lf", &nota2);
