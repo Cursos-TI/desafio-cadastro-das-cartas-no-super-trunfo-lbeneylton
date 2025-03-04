5341b4a0ce5de939e07efe341191ff57a9b7755e
lbeneylton <47756391+lbeneylton@users.noreply.github.com>
2025-03-04 13:19:51 +0000

    Ajustei o código de verificação de desconto baseado na idade e na renda

--- Logica/Aventureiro/exemplos/verificar_idade_e_renda.c
+++ Logica/Aventureiro/exemplos/verificar_idade_e_renda.c
@@ -6,15 +6,19 @@ A pessoa deve ter mais de 60 anos ou menos de 18 anos e ter uma renda mensal aba
 */
 
 int main(){
-    int idade = 25;
-    unsigned float renda = 1800;
+    int idade = 80;
+    float renda = 25000;
 
     if (idade>60 || idade<18){
-        if(renda<200){
-            printf("Você tem direito ao desconto")
+        if(renda<2000){
+            printf("Você tem direito ao desconto");
+
+        }else{
+            printf("Você não tem direito ao desconto pois sua renda é maior que R$ 2000,00");
         }
+        
     }else{
-        printf("Você não tem direito ao desconto");
+        printf("Você não tem direito ao desconto pois não atende aos requisitos de idade");
     }
     printf("\n\n\n");
 
